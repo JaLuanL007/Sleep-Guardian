e5f54eefb04584ffa2d32155b051448ea7d3c8d8
JaLuan Love <twgjay@gmail.com>
2023-07-28 14:47:34 -0700

    cheese2

--- heartrate watch Watch App/ContentView.swift
+++ heartrate watch Watch App/ContentView.swift
@@ -7,43 +7,45 @@
 
 import SwiftUI
 import Combine
+import HealthKit
+
 struct WelcomePage: View {
     
         var body: some View {
-            NavigationStack {
-
-            HStack {
-                Image("SleepGuardian")
-                    .imageScale(.large)
-                    .foregroundColor(.purple)
-                Text("Welcome")
-                    .font(.custom(
-                        "SecularOne",
-                        fixedSize: 20))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-            }
-            Spacer();
-            Text("Sleep Guardian is your smart sleep assistant aimed towards combating Sleep Paralysis")
-                .font(.custom(
-                    "SecularOne", fixedSize:10
-                ))
-              //                      .fontWeight(.heavy)
-            //                        .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .lineSpacing(3)
-            Spacer()
-                    VStack {
-                        NavigationLink("BEGIN") {
-                            InfoPage()
-                                                }
-                                .foregroundColor(Color.white )
-                                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                                .cornerRadius(25)
-                        
-                                        
-                    }
+            VStack {
+                NavigationStack {
+                
+                VStack {
+                    Image("SleepGuardian")
+                        .imageScale(.large)
+                        .foregroundColor(.purple)
+                 
+                    Spacer()
+                    Text("Welcome To Sleep Guardian")
+                        .font(.custom(
+                            "SecularOne",
+                            fixedSize: 13))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
                 }
+                    
+                
+                        VStack {
+                            Spacer()
+                            NavigationLink("BEGIN") {
+                                AllowAccess()
+                                                    }
+                                    .foregroundColor(Color.white )
+                                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                                    .cornerRadius(25)
+                Spacer()
+                            
+                                            
+                        }
+                }
+                Spacer()
+            }
                 
             }
         }
@@ -54,206 +56,142 @@ struct WelcomePage_Previews: PreviewProvider {
             }
         }
 
-struct InfoPage: View {
+struct AllowAccess: View {
     var body: some View {
         NavigationStack{
             VStack {
-                Text("To get started, we’d like to ask you questions. Your answers will help us combat your sleep paralysis")
+                Image("SleepGuardian")
+                    .imageScale(.large)
+                    .foregroundColor(.purple)
+                Spacer()
+                Text("Allow SleepGuardian to access your heart rate information")
                     .font(.custom(
-                        "SecularOne", fixedSize:12
-                    ))
-                    .fontWeight(.heavy)
+                        "SecularOne",
+                        fixedSize: 13))
+                    .fontWeight(.bold)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
-                    .lineSpacing(3)
                 Spacer()
-                NavigationLink("NEXT") {
-                    Age()
+                HStack {
+                    NavigationLink("DON'T ALLOW") {
+                        AllowAccess2()
+                    }
+                    .font(.custom(
+                        "SecularOne",
+                        fixedSize: 9))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white )
+                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                    .cornerRadius(25)
+                    Spacer()
+                    NavigationLink("ALLOW") {
+                        HeartrateSensor()
+                    }
+                    .font(.custom(
+                        "SecularOne",
+                        fixedSize: 11))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white )
+                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                    .cornerRadius(25)
                 }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-            }
             }
         }
     }
+}
 
-struct InfoPage_Previews: PreviewProvider {
+struct AllowAccess_Previews: PreviewProvider {
     static var previews: some View {
-        InfoPage()
+        AllowAccess()
     }
 }
-        
-struct Age: View {
-    var body: some View{
-        VStack {
-            NavigationStack{
-                VStack{
-                    Text("What's Your Age?")
-                        .fontWeight(.bold)
-                    Spacer()
-                    TextField("Enter Age", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    NavigationLink("NEXT") {
-                        Height()
+
+struct AllowAccess2: View {
+    var body: some View {
+        NavigationStack{
+            VStack {
+                Image("SleepGuardian")
+                    .imageScale(.large)
+                    .foregroundColor(.purple)
+                Spacer()
+                Text("SleepGuardian requires access to your heart rate information in order to combat sleep paralysis")
+                    .font(.custom(
+                        "Baloo Tammudu",
+                        fixedSize: 11))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                HStack{
+                    NavigationLink("DON'T ALLOW") {
+                        WelcomePage()
+                    }
+                    .font(.custom(
+                        "Baloo Tammudu",
+                        fixedSize: 9))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white )
+                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                    .cornerRadius(25)
+                    NavigationLink("OK") {
+                        HeartrateSensor()
                     }
                     .foregroundColor(Color.white )
                     .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
                     .cornerRadius(25)
                 }
             }
-                    }
+        }
     }
 }
 
-struct Age_Previews: PreviewProvider {
+struct AllowAccess2_Previews: PreviewProvider {
     static var previews: some View {
-        Age()
+        AllowAccess2()
     }
 }
 
-struct Height: View {
-var body: some View{
-    VStack {
-        NavigationStack{
-            VStack{
-                Text("What's Your Height?")
-                    .fontWeight(.bold)
-                Spacer()
-                TextField("Enter Height", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                Spacer()
-                NavigationLink("NEXT") {
-                    Weight()
-                }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-            }
-        }
-                }
-}
-}
-
-struct Height_Previews: PreviewProvider {
-static var previews: some View {
- Height()
-}
-}
-
-struct Weight: View {
-var body: some View{
-    VStack {
-        NavigationStack{
-            VStack{
-                Text("What's Your Weight?")
-                    .fontWeight(.bold)
-                Spacer()
-                TextField("Enter Weight", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                Spacer()
-                NavigationLink("NEXT") {
-                    FitnessLevel()
-                }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-            }
-        }
-                }
-}
-}
-
-struct Weight_Previews: PreviewProvider {
-static var previews: some View {
-Weight()
-}
-}
-
-struct FitnessLevel: View {
-var body: some View{
-    VStack {
-        NavigationStack{
-            VStack{
-                Spacer()
-                Text("What Is Your Level Of Fitness?")
-                  
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(3)
-                Spacer()
-                NavigationLink("Not Very Active") {
-                   OK()
-                }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-                Spacer()
-                NavigationLink("Slightly Active") {
-                    OK()
-                }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-                Spacer()
-                NavigationLink("Very Active") {
-                    OK()
-                }
-                .foregroundColor(Color.white )
-                .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                .cornerRadius(25)
-                Spacer()
-            }
-        }
-        }
-}
-}
-
-struct FitnessLevel_Previews: PreviewProvider {
-static var previews: some View {
-FitnessLevel()
-}
-}
-
-struct OK: View{
-    var body: some View{
-        NavigationStack{
-            VStack{
-                Text("OK ")
-                NavigationLink("Chicken tendies"){
-                    StackOverflowTests()
-                }
-            }
-        }
-    }
-    
-    struct OK_Previews: PreviewProvider {
-        static var previews: some View {
-            OK()
-        }
-    }
-    
-}
-
-                
-
-struct StackOverflowTests: View {
-    @State var numOfPeopl = "Enter Age"
+struct HeartrateSensor: View {
+    @State var heartRate: Double = 0
     
     var body: some View {
-        TextField("Enter Age", text: $numOfPeopl)
-            .onReceive(Just(numOfPeopl)) { newValue in
-                let filtered = newValue.filter { "0123456789".contains($0) }
-                if filtered != newValue {
-                    self.numOfPeopl = filtered
-                    
-                    struct StackOverflowTests_Previews: PreviewProvider{
-                        typealias Previews = <#type#>
-                        
-                        @State var numOfPeopl: <# Type #>{
-                            StackOverflowTests()
-                        }
-                    }
-                }
+        VStack {
+            Text("Heart Rate: \(heartRate)")
+                .font(.title)
+            
+            Button(action: {
+                self.getHeartRate()
+            }) {
+                Text("Get Heart Rate")
+                    .font(.headline)
             }
+        }
+    }
+    
+    func getHeartRate() {
+        let healthStore = HKHealthStore()
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+        let date = Date()
+        let predicate = HKQuery.predicateForSamples(withStart: date.addingTimeInterval(-60), end: date, options: .strictEndDate)
+        let query = HKStatisticsQuery(quantityType: heartRateType, quantitySamplePredicate: predicate, options: .discreteAverage) { _, result, _ in
+            guard let result = result, let quantity = result.averageQuantity() else {
+                return
+            }
+            self.heartRate = quantity.doubleValue(for: HKUnit(from: "count/min"))
+        }
+        healthStore.execute(query)
+    }
+}
+
+
+struct HeartrateSensor_Previews: PreviewProvider {
+    static var previews: some View {
+        HeartrateSensor()
     }
 }
