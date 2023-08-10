cda5cd9e18985e44bf2e72c14bd92a1aff116081
JaLuan Love <twgjay@gmail.com>
2023-08-10 10:35:42 -0700

    cheese3

--- heartrate watch Watch App/ContentView.swift
+++ heartrate watch Watch App/ContentView.swift
@@ -8,100 +8,91 @@
 import SwiftUI
 import Combine
 import HealthKit
+import Foundation
+
 
 struct WelcomePage: View {
-    
     var body: some View {
-        VStack {
-            NavigationStack {
-                
-                VStack {
-                    Image("SleepGuardian")
-                        .imageScale(.large)
-                        .foregroundColor(.purple)
-                    
-                    Spacer()
-                    Text("Welcome To Sleep Guardian")
-                        .font(.custom(
-                            "SecularOne",
-                            fixedSize: 13))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                }
-                
-                
-                VStack {
-                    Spacer()
-                    NavigationLink("BEGIN") {
-                        AllowAccess()
-                    }
-                    .foregroundColor(Color.white )
-                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                    .cornerRadius(25)
-                    Spacer()
-                    
-                    
-                }
-            }
-            Spacer()
-        }
-    }
-}
-
-struct WelcomePage_Previews: PreviewProvider {
-            static var previews: some View {
-                WelcomePage()
-            }
-        }
-
-struct AllowAccess: View {
-    var body: some View {
-        NavigationStack{
+        NavigationView {
             VStack {
                 Image("SleepGuardian")
                     .imageScale(.large)
                     .foregroundColor(.purple)
+
                 Spacer()
-                Text("Allow SleepGuardian to access your heart rate information")
-                    .font(.custom(
-                        "SecularOne",
-                        fixedSize: 13))
+
+                Text("Welcome To Sleep Guardian")
+                    .font(.custom("SecularOne", size: 13))
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
+
                 Spacer()
-                HStack {
-                    NavigationLink("DON'T ALLOW") {
-                        AllowAccess2()
-                    }
-                    .font(.custom(
-                        "SecularOne",
-                        fixedSize: 9))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white )
+
+                NavigationLink("BEGIN", destination: AllowAccess())
+                    .foregroundColor(Color.white)
                     .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
                     .cornerRadius(25)
-                    Spacer()
-                    NavigationLink("ALLOW") {
-                        OK()
-                    }
-                    .font(.custom(
-                        "SecularOne",
-                        fixedSize: 11))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white )
-                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
-                    .cornerRadius(25)
-                }
+
+                Spacer()
             }
         }
     }
 }
+
+struct  WelcomePage_Previews:PreviewProvider{
+    static var previews: some View{
+        WelcomePage()
+    }
+}
+
+struct AllowAccess: View {
+//    @State private var isAuthorized = false
+//    private let healthKitManager = HealthKitManager()
+
+    var body: some View {
+        VStack {
+            Image("SleepGuardian")
+                .imageScale(.large)
+                .foregroundColor(.purple)
+
+            Spacer()
+
+            Text("Allow SleepGuardian to access your heart rate information")
+                .font(.custom("SecularOne", size: 13))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+
+            Spacer()
+
+            HStack {
+                NavigationLink("DON'T ALLOW", destination: WelcomePageReturn())
+                    .font(.custom("SecularOne", size: 9))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                    .cornerRadius(25)
+
+                Spacer()
+
+                NavigationLink("ALLOW", destination: BPMC())
+                    .font(.custom("SecularOne", size: 11))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+                    .cornerRadius(25)
+            }
+        }
+//        .onAppear {
+//            healthKitManager.requestHeartRateAuthorization { success, _ in
+//                isAuthorized = success
+            }
+        }
+//    }
+//}
+
+// ... (Other views remain the same)
 
 struct AllowAccess_Previews: PreviewProvider {
     static var previews: some View {
@@ -139,7 +130,7 @@ struct AllowAccess2: View {
                     .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
                     .cornerRadius(25)
                     NavigationLink("OK") {
-                        OK()
+                        BPMC()
                     }
                     .foregroundColor(Color.white )
                     .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
@@ -199,79 +190,225 @@ struct WelcomePageReturn_Previews: PreviewProvider {
                 WelcomePageReturn()
             }
         }
-struct OK: View{
-    var body: some View {
-        Text("76")
-    }
+
+struct BPMC: View{
+    var body: some View{
+        VStack{
+            Text("RESTING BPM")
+            Spacer()
+            HStack{
+                Image("Heart")
+                Text("80")
+            }
+        
+            Text("Heart Rate")
+            Spacer()
+            NavigationLink("NEXT") {
+                ContentView2()
+            }
+            .foregroundColor(Color.white )
+            .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+            .cornerRadius(25)
+            Spacer()
+        }
+        
+            Spacer()
+            NavigationLink("BEGIN") {
+                ContentView3()
+            }
+            .foregroundColor(Color.white )
+            .background(Color(red: 80 / 255, green: 1 / 255, blue: 142 / 255))
+            .cornerRadius(25)
+            Spacer()
+        }
     }
 
-struct OK_Previews: PreviewProvider {
+struct BPMC_Previews: PreviewProvider {
             static var previews: some View {
-                OK()
+                BPMC()
             }
         }
 
+import WatchKit
+import Foundation
+import SwiftUI
+import UIKit
 
-struct HeartrateSensor: View{
-    @State  var heartRate: Double = 0.0
-    @State  var isAuthorized = false
+struct ContentView2: View {
     var body: some View {
-        VStack {
-            Text("Heart Rate: \(Int(heartRate))")
-                .padding()
-            Button(action: {
-                authorizeHealthKit()
-            }) {
-                Text("Request Authorization")
-            }
-            .padding()
-            .disabled(isAuthorized)
-        }
+       
+        
+        
+        Button(action: {
+            // Call the method to trigger vibration
+            vibrateWatch()
+        }, label: {
+            Text("Vibrate Watch")
+        })
+        
     }
-    func authorizeHealthKit() {
-        let healthStore = HKHealthStore()
-        let readTypes: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .heartRate)!]
-        healthStore.requestAuthorization(toShare: nil, read: readTypes) { (success, error) in
-            if success {
-                isAuthorized = true
-                startHeartRateReading()
-            } else {
-                // Handle error
-            }
+    
+    // Function to trigger vibration
+    private func vibrateWatch() {
+        if #available(watchOS 7.0, *) {
+            WKInterfaceDevice.current().play(.notification)
+        } else {
+            // Fallback for older watchOS versions (optional)
+            print("Vibration not supported on this watchOS version.")
         }
-    }
-    func startHeartRateReading() {
-        guard HKHealthStore.isHealthDataAvailable() else { return }
-        let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
-        let query = HKObserverQuery(sampleType: heartRateType, predicate: nil) { (query, completionHandler, error) in
-            if error != nil {
-                // Handle error
-                return
-            }
-            self.readHeartRate()
-        }
-        let healthStore = HKHealthStore()
-        healthStore.execute(query)
-    }
-    func readHeartRate() {
-        let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
-        let datePredicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
-        let query = HKSampleQuery(sampleType: heartRateType, predicate: datePredicate, limit: 1, sortDescriptors: nil) { (query, samples, error) in
-            if let samples = samples as? [HKQuantitySample] {
-                if let sample = samples.first {
-            //        let heartRateUnit = HKUnit(from: count/min )
-               //     self.heartRate = sample.quantity.doubleValue(for: heartRateUnit)
-                }
-            }
-        }
-        let healthStore = HKHealthStore()
-        healthStore.execute(query)
     }
 }
 
-struct HeartrateSensor_Previews: PreviewProvider {
-    static var previews: some View{
-        HeartrateSensor()
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
     }
 }
+
+
+struct ContentView3: View {
+    @State private var isVibrating = false // Track vibration state
+    
+    // Property to hold the reference to the vibration timer
+    private var vibrationTimer: Timer?
+    
+    var body: some View {
+        Button(action: {
+            // Toggle vibration state and start/stop vibration loop
+            isVibrating.toggle()
+            if isVibrating {
+                startVibrationLoop()
+            } else {
+                stopVibrationLoop()
+            }
+        }, label: {
+            Text(isVibrating ? "Start Vibration" : "Stop Vibration")
+        })
+    }
+    
+    // Function to trigger vibration
+    private func vibrateWatch() {
+        if #available(watchOS 7.0, *) {
+            WKInterfaceDevice.current().play(.notification)
+        } else {
+            // Fallback for older watchOS versions (optional)
+            print("Vibration not supported on this watchOS version.")
+        }
+    }
+    
+    // Function to start the vibration loop
+    private func startVibrationLoop() {
+        // Start a loop to repeatedly vibrate
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            vibrateWatch()
+        }
+    }
+    
+    // Function to stop the vibration loop
+    private func stopVibrationLoop() {
+        // Stop the vibration loop (invalidate the timer)
+        
+       
+    }
+}
+
+struct ContentView3_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView3()
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //struct HealthKitManager: View {
+    //
+    //    var body: some View {
+    //
+    //        class HealthKitManager {
+    //            let healthStore = HKHealthStore()
+    //
+    //            func requestHeartRateAuthorization(completion: @escaping (Bool, Error?) -> Void) {
+    //                let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+    //
+    //                return healthStore.requestAuthorization(toShare: [], read: [heartRateType]) { success, error in
+    //                    completion(success, error)
+    //                }
+    //            }
+    //
+    //            func startHeartRateReading(completion: @escaping (Double) -> Void) {
+    //                guard HKHealthStore.isHealthDataAvailable() else { return }
+    //                let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
+    //                let datePredicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
+    //                let query = HKSampleQuery(sampleType: heartRateType, predicate: datePredicate, limit: 1, sortDescriptors: nil) { query, samples, error in
+    //                    if let samples = samples as? [HKQuantitySample], let sample = samples.first {
+    //                        let heartRateUnit = HKUnit(from: "count/min")
+    //                        let heartRate = sample.quantity.doubleValue(for: heartRateUnit)
+    //                        completion(heartRate)
+    //                    } else {
+    //                        completion(0.0)
+    //                    }
+    //                }
+    //                healthStore.execute(query)
+    //
+    //            }
+    //
+    //        }
+    //
+    //    }
+    //}
+    //
+    //struct HealthKitManager_Previews: PreviewProvider {
+    //            static var previews: some View {
+    //                return HealthKitManager()
+    //            }
+    //        }
 
